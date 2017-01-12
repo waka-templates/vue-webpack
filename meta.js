3570ab7c2fca284751f8aea54e3884b88a9e98c8
dwqs <461147874@qq.com>
2017-01-12 11:54:53 +0800

    fix

--- meta.js
+++ meta.js
@@ -1,19 +1,20 @@
 module.exports = {
   "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project name"
-    },
-    "description": {
-      "type": "string",
-      "required": false,
-      "message": "Project description",
-      "default": "A new Vue.js project"
-    },
-    "author": {
-      "type": "string",
-      "message": "Author"
-    }
+      "name"       : {
+          "type"    : "string",
+          "required": true,
+          "message" : "Project name"
+      },
+      "description": {
+          "type"    : "string",
+          "required": false,
+          "message" : "Project description",
+          "default" : "A new Vue.js project"
+      },
+      "author"     : {
+          "type"   : "string",
+          "message": "Author"
+      }
+  },
   "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
-};
+}
