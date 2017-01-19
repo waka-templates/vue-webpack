923c60be194650961da9d32f0649a4c07712cd97
dwqs <461147874@qq.com>
2017-01-19 19:03:15 +0800

    update config

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -3,15 +3,16 @@
 let webpack = require('webpack');
 let WebpackDevServer = require('webpack-dev-server');
 
-let config = require('./webpack.dev.config.js');
+let webpackDevConfig = require('./webpack.dev.config.js');
+let config = require('../config/index');
 
-let compiler = webpack(config);
-let server = new WebpackDevServer(compiler, config.devServer);
+let compiler = webpack(webpackDevConfig);
+let server = new WebpackDevServer(compiler, webpackDevConfig.devServer);
 
 server.listen(3000, 'localhost', (err) => {
     if(err){
         console.error(err);
         return;
     }
-    console.log('open your browser visit localhost:3000\n');
+    console.log(`open your browser and visit localhost:${config.dev.port}\n`);
 });
