7f741658f213f61355aa66dfb2222530b12103a5
dwqs <461147874@qq.com>
2017-01-19 15:14:41 +0800

    update dev config

--- template/build/webpack.dev.config.js
+++ template/build/webpack.dev.config.js
@@ -10,13 +10,12 @@ devConfig.plugins = (devConfig.plugins || []).concat([
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: config.dev.env
-        }
+        'process.env': config.dev.env
     }),
     new webpack.NoErrorsPlugin()
 ]);
 
+// see https://webpack.github.io/docs/webpack-dev-server.html
 devConfig.devServer = {
     hot: true,
     noInfo: false,
@@ -37,15 +36,14 @@ module.exports = Object.assign({},devConfig,{
         app:[
             "webpack/hot/dev-server",
             "webpack-dev-server/client?http://localhost:3000/",
-            path.resolve(__dirname, '../client/index.js')
+            path.resolve(__dirname, '../src/index.js')
         ]
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, '../public/assets/'),
-        //将publicPath设置为webpack-dev-server服务器下资源目录的绝对路径,不然不会是局部更新,而是重新记载页面
         publicPath:"http://localhost:3000/assets",
         sourceMapFilename: '[file].map'
     },
-    devtool:'#cheap-module-eval-source-map'  //防中文乱码
+    devtool:'#cheap-module-eval-source-map'
 });
