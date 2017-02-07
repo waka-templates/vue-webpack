35945ebf3cae6c7dc2e97840f8895f6e18bd0e6b
dwqs <461147874@qq.com>
2017-02-07 16:30:24 +0800

    update karma config

--- template/test/karma.conf.js
+++ template/test/karma.conf.js
@@ -13,11 +13,38 @@ module.exports = function(config) {
         // list of files / patterns to load in the browser
         files: ['./unit/**/*.spec.js'],
 
+        plugins: [
+         'karma-chrome-launcher',
+         'karma-mocha',
+         'karma-sourcemap-loader',
+         'karma-webpack',
+         'karma-mocha-reporter',
+         'karma-chai'
+       ],
+
         preprocessors: {
-          './unit/**/*.spec.js': ['webpack']
+          './unit/**/*.spec.js': ['webpack','sourcemap']
         },
 
-        reporters: ['progress', 'coverage'],
+        reporters: ['progress','mocha'],
+
+        mochaReporter: {
+              colors: {
+                success: 'blue',
+                info: 'bgGreen',
+                warning: 'cyan',
+                error: 'bgRed'
+              },
+              symbols: {
+                success: '+',
+                info: '#',
+                warning: '!',
+                error: 'x'
+              }
+        },
+
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
         // web server port
         port: 9876,
@@ -26,7 +53,14 @@ module.exports = function(config) {
         colors: true,
 
         // start these browsers
-        browsers: ['Chrome'],
+        browsers: ['Chrome','Chrome_without_security'],
+
+        customLaunchers: {
+            Chrome_without_security: {
+                    base: 'Chrome',
+                    flags: ['--disable-web-security']
+            }
+        },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
@@ -34,6 +68,7 @@ module.exports = function(config) {
 
         // webpack配置
         webpack: {
+            devtool: 'inline-source-map',
             module: {
                 loaders: [
                     {
@@ -63,9 +98,8 @@ module.exports = function(config) {
             ]
         },
 
-        coverageReporter: {
-            type : 'html',
-            dir : 'coverage/'
+        webpackServer: {
+            noInfo: true
         }
     })
 };
